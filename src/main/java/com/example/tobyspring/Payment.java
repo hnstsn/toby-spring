d582b9d7077d458df291ba74f48b8053dcee6046
WANSEO\hnsts <hnstsn@naver.com>
2024-07-30 22:44:20 +0900

    payment service, payment 생성

---
+++ src/main/java/com/example/tobyspring/Payment.java
@@ -0,0 +1,58 @@
+package com.example.tobyspring;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Payment {
+    private Long orderId;   // 주문번호
+    private String currency;   // 통화 종류
+    private BigDecimal foreignCurrencyAmount;    // 통화 환산 금액
+    private BigDecimal exRate;  // 총 금액
+    private BigDecimal convertedAmount;     // 환산된 금액
+    private LocalDateTime validUntil;    // 유효시간
+
+    public Payment(Long orderId, String currency, BigDecimal foreignCurrencyAmount, BigDecimal exRate, BigDecimal convertedAmount, LocalDateTime validUntil) {
+        this.orderId = orderId;
+        this.currency = currency;
+        this.foreignCurrencyAmount = foreignCurrencyAmount;
+        this.exRate = exRate;
+        this.convertedAmount = convertedAmount;
+        this.validUntil = validUntil;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public BigDecimal getForeignCurrencyAmount() {
+        return foreignCurrencyAmount;
+    }
+
+    public BigDecimal getExRate() {
+        return exRate;
+    }
+
+    public BigDecimal getConvertedAmount() {
+        return convertedAmount;
+    }
+
+    public LocalDateTime getValidUntil() {
+        return validUntil;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "orderId=" + orderId +
+                ", currency='" + currency + '\'' +
+                ", foreignCurrencyAmount=" + foreignCurrencyAmount +
+                ", exRate=" + exRate +
+                ", convertedAmount=" + convertedAmount +
+                ", validUntil=" + validUntil +
+                '}';
+    }
+}
