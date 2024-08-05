47ec2d1d359771945d6885c655c8bf691b543a67
WANSEO\hnsts <hnstsn@naver.com>
2024-08-05 22:53:42 +0900

    클래스 분리

--- src/main/java/com/example/tobyspring/PaymentService.java
+++ src/main/java/com/example/tobyspring/PaymentService.java
@@ -12,44 +12,23 @@ import java.time.LocalDateTime;
 import java.util.stream.Collectors;
 
 public class PaymentService {
+//    private final WebApiExRateProvider exRateProvider;
+    private final SimpleExRateProvider exRateProvider;
+
+    public PaymentService() {
+//        this.exRateProvider = new WebApiExRateProvider();
+        this.exRateProvider = new SimpleExRateProvider();
+    }
+
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) {
-        BigDecimal exRate = getExRate(currency);
+//        BigDecimal exRate = exRateProvider.getWebExRate(currency);
+        BigDecimal exRate = exRateProvider.getExRate(currency);
         BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
         LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
-        
         return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
     }
 
     // 환율 가져오기
-    private BigDecimal getExRate(String currency) {
-        String url = "https://open.er-api.com/v6/latest/" + currency;
-        URI uri = null;
-        try {
-            uri = new URI(url);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-
-        String response;
-        try {
-            HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
-            try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-                response = br.lines().collect(Collectors.joining());
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        BigDecimal exRate;
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            ExRateData data = mapper.readValue(response, ExRateData.class);
-            exRate = data.rates().get("KRW");
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-        return exRate;
-    }
 
     public static void main(String[] args) {
         PaymentService paymentService = new PaymentService();
---
+++ src/main/java/com/example/tobyspring/WebApiExRateProvider.java
@@ -0,0 +1,45 @@
+package com.example.tobyspring;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.stream.Collectors;
+
+public class WebApiExRateProvider {
+    public BigDecimal getWebExRate(String currency) {
+        String url = "https://open.er-api.com/v6/latest/" + currency;
+        URI uri = null;
+        try {
+            uri = new URI(url);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        String response;
+        try {
+            HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                response = br.lines().collect(Collectors.joining());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        BigDecimal exRate;
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            ExRateData data = mapper.readValue(response, ExRateData.class);
+            exRate = data.rates().get("KRW");
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        return exRate;
+    }
+}
