63e002691aa4e00c10729f621cf6589b93ccf741
WANSEO\hnsts <hnstsn@naver.com>
2024-08-07 21:38:04 +0900

    의존관계 주입. BeanFactory 사용(스프링 IoC/DI)

--- src/main/java/com/example/tobyspring/Client.java
+++ src/main/java/com/example/tobyspring/Client.java
@@ -1,12 +1,14 @@
 package com.example.tobyspring;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.math.BigDecimal;
 
 public class Client {
     public static void main(String[] args) {
-//        PaymentService paymentService = new PaymentService(new WebApiExRateProvider()); // 여기서 설정
-        ObjectFactory objectFactory = new ObjectFactory();
-        PaymentService paymentService = objectFactory.paymentService();
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
+        PaymentService paymentService = beanFactory.getBean(PaymentService.class);
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println("payment = " + payment);
     }
