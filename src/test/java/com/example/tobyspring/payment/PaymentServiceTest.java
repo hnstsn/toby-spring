ea266349fa66ae0143ec9ea532bf9c39270ae424
WANSEO\hnsts <hnstsn@naver.com>
2024-08-13 22:54:15 +0900

    test code refactoring

--- src/test/java/com/example/tobyspring/payment/PaymentServiceTest.java
+++ src/test/java/com/example/tobyspring/payment/PaymentServiceTest.java
@@ -1,29 +1,31 @@
 package com.example.tobyspring.payment;
 
-import com.example.tobyspring.exrate.WebApiExRateProvider;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
-import static org.assertj.core.api.Assertions.*;
+import static java.math.BigDecimal.valueOf;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 class PaymentServiceTest {
 
     @Test
-    void prepare() {
-        PaymentService paymentService = new PaymentService(new WebApiExRateProvider());
-        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.valueOf(10));
+    void convertedAmount() {
+        testAmount(valueOf(500), valueOf(5_000));
+        testAmount(valueOf(3_000), valueOf(30_000));
+
+//        // 유효시간 계산
+//        assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
+    }
+
+    private static void testAmount(BigDecimal exRate, BigDecimal convertedAmount) {
+        PaymentService paymentService = new PaymentService(new ExRateProviderStub(exRate));
+        Payment payment = paymentService.prepare(1L, "USD", valueOf(10));
         // 환율정보
-        assertThat(payment.getExRate()).isNotNull();
+        assertThat(payment.getExRate()).isEqualByComparingTo(exRate);
 
         // 원화환산금액 계산
-        assertThat(payment.getConvertedAmount())
-                .isEqualTo(payment.getExRate().multiply(payment.getForeignCurrencyAmount()));
-
-        // 유효시간 계산
-        assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
+        assertThat(payment.getConvertedAmount()).isEqualByComparingTo(convertedAmount);
     }
 }
