4913d8b9fb9f8845f971bb7ef8c004bf60dff0cf
WANSEO\hnsts <hnstsn@naver.com>
2024-08-15 21:44:02 +0900

    스프링 기술 RestTemplate 활용

--- src/main/java/com/example/tobyspring/PaymentConfig.java
+++ src/main/java/com/example/tobyspring/PaymentConfig.java
@@ -1,13 +1,16 @@
 package com.example.tobyspring;
 
-import com.example.tobyspring.api.*;
+import com.example.tobyspring.api.ApiTemplate;
+import com.example.tobyspring.api.ErApiExRateExtractor;
+import com.example.tobyspring.api.SimpleApiExecutor;
 import com.example.tobyspring.exrate.CachedExRateProvider;
+import com.example.tobyspring.exrate.RestTemplateExRateProvider;
 import com.example.tobyspring.payment.ExRateProvider;
-import com.example.tobyspring.exrate.WebApiExRateProvider;
 import com.example.tobyspring.payment.PaymentService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.Clock;
 
@@ -31,8 +34,14 @@ public class PaymentConfig {
     }
 
     @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
     public ExRateProvider exRateProvider() {
-        return new WebApiExRateProvider(apiTemplate());
+//        return new WebApiExRateProvider(apiTemplate());
+        return new RestTemplateExRateProvider(restTemplate());
     }
 
     @Bean
