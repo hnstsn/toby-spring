c6375ef28f22b107000afbce77815cf231d73e12
WANSEO\hnsts <hnstsn@naver.com>
2024-08-21 21:56:22 +0900

    Order entity에서 JPA 종속성 제거

--- src/main/java/com/example/tobyspring/order/Order.java
+++ src/main/java/com/example/tobyspring/order/Order.java
@@ -1,17 +1,10 @@
 package com.example.tobyspring.order;
 
-import jakarta.persistence.*;
-
 import java.math.BigDecimal;
 
-@Entity
-@Table(name = "orders")
 public class Order {
-    @Id
-    @GeneratedValue
     private Long id;    // 주문번호
 
-    @Column(unique = true)
     private String no;  // 주문번호(유저용)
 
     private BigDecimal total;   // 총 지불 금액
