14be34d49f3dadbb214ff0769764156e0b51b575
swch@isu.co.kr <swch@isu.co.kr>
2024-08-23 08:06:55 +0900

    OrderService에 Transaction proxy 적용

--- src/main/java/com/example/tobyspring/config/OrderConfig.java
+++ src/main/java/com/example/tobyspring/config/OrderConfig.java
@@ -3,6 +3,8 @@ package com.example.tobyspring.config;
 import com.example.tobyspring.data.JdbcOrderRepository;
 import com.example.tobyspring.order.OrderRepository;
 import com.example.tobyspring.order.OrderService;
+import com.example.tobyspring.order.OrderServiceImpl;
+import com.example.tobyspring.order.OrderServiceTxProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -14,8 +16,8 @@ import javax.sql.DataSource;
 @Import(DataConfig.class)
 public class OrderConfig {
     @Bean
-    public OrderService orderService(PlatformTransactionManager transactionManager, OrderRepository orderRepository) {
-        return new OrderService(orderRepository, transactionManager);
+    public OrderService orderService(OrderRepository orderRepository, PlatformTransactionManager transactionManager) {
+        return new OrderServiceTxProxy(new OrderServiceImpl(orderRepository), transactionManager);
     }
 
     @Bean
