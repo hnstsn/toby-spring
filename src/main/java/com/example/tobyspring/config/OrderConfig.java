b16d77b699a662418e83f8f3ee1ca90c94800534
WANSEO\hnsts <hnstsn@naver.com>
2024-08-23 23:58:08 +0900

    @Transactional 활용

--- src/main/java/com/example/tobyspring/config/OrderConfig.java
+++ src/main/java/com/example/tobyspring/config/OrderConfig.java
@@ -4,20 +4,20 @@ import com.example.tobyspring.data.JdbcOrderRepository;
 import com.example.tobyspring.order.OrderRepository;
 import com.example.tobyspring.order.OrderService;
 import com.example.tobyspring.order.OrderServiceImpl;
-import com.example.tobyspring.order.OrderServiceTxProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 @Configuration
 @Import(DataConfig.class)
+@EnableTransactionManagement
 public class OrderConfig {
     @Bean
-    public OrderService orderService(OrderRepository orderRepository, PlatformTransactionManager transactionManager) {
-        return new OrderServiceTxProxy(new OrderServiceImpl(orderRepository), transactionManager);
+    public OrderService orderService(OrderRepository orderRepository) {
+        return new OrderServiceImpl(orderRepository);
     }
 
     @Bean
